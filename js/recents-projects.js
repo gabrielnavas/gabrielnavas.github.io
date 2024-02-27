0586490fb354d3e089f43462af63ef027ec7e073
navas <gabrielnavas@email.com>
2024-02-27 15:54:06 -0300

    add random colors

--- js/recents-projects.js
+++ js/recents-projects.js
@@ -1,4 +1,4 @@
-function createProjectListItem(title, githubUrl, description, tech) {
+function createProjectListItem(title, githubUrl, description, techs) {
   const projectList = document.querySelector('.recents-projects-list');
 
   const listItem = document.createElement('li');
@@ -17,18 +17,38 @@ function createProjectListItem(title, githubUrl, description, tech) {
   subtitleDiv.classList.add('subtitle');
   subtitleDiv.textContent = description;
 
-  const techList = document.createElement('ul');
-  techList.classList.add('tech-list');
+  const techList = createTechList(techs)
 
-  const techItem = document.createElement('li');
-  techItem.classList.add('tech-item');
-  techItem.textContent = tech;
-
-  techList.appendChild(techItem);
   link.appendChild(titleDiv);
   link.appendChild(subtitleDiv);
   link.appendChild(techList);
   listItem.appendChild(link);
 
   projectList.appendChild(listItem);
+}
+
+function randomColor() {
+  const index = Math.floor(Math.random() * 6);
+  const colors = ['red', 'green', 'blue', 'magenta', 'cyan', 'yellow'];
+  return colors[index];
+}
+
+function createTechList(techs) {
+  const techList = document.createElement('ul');
+  techList.classList.add('tech-list');
+
+  const techItems = techs.map(tech => {
+    const techItem = document.createElement('li');
+    techItem.classList.add('tech-item');
+    const color = randomColor();
+    techItem.classList.add(`${color}`);
+    techItem.textContent = tech;
+    return techItem;
+  });
+
+  for (var techItem of techItems) {
+    techList.appendChild(techItem);
+  }
+
+  return techList
 }
